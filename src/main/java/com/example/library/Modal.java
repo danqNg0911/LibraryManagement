f0e82e454a2d4e524e91fc4dba8592c5157457c0
Hungvpp <144545805+hungpham711@users.noreply.github.com>
2024-10-06 11:21:12 +0700

    Add files via upload

---
+++ src/main/java/com/example/library/Modal.java
@@ -0,0 +1,18 @@
+package com.example.library;
+
+public class Modal {
+    public static float cacll(long number1, long number2, String operator) {
+        switch (operator) {
+            case "x":
+                return number1 * number2;
+            case "+":
+                return number1 + number2;
+            case "-":
+                return number1 - number2;
+            case "/":
+                if (number2 == 0) return 0;
+                return (float) number1 / number2;
+        }
+        return 0;
+    }
+}
