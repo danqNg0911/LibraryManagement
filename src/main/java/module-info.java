2aacb1b8abc50c44ca750a5e2645db43685a60fc
Hungvpp <144545805+Hungpham711@users.noreply.github.com>
2024-10-06 11:21:12 +0700

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.testfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.testfx to javafx.fxml;
+    exports com.example.testfx;
+}
