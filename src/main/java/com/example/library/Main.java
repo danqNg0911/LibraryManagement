fe2a13bf3b0f9cb3cf75d498cf48d405a37f5ead
Nguyen Huu Hai Dang <2005nguyendang@gmail.com>
2024-10-06 23:27:34 +0700

    Add CreateAccount

--- src/main/java/com/example/library/Main.java
+++ src/main/java/com/example/library/Main.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/Sign_In.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/SignIn.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 500);
         stylesheet("stylesheet (css)/style.css", scene);
         stylesheet("stylesheet (css)/login.css", scene);
