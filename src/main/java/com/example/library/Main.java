b19b317974d1b8582d348b00dee0c35fb2f39793
Hungpham711 <viethungbi711@gmail.com>
2024-10-06 11:40:53 +0700

    Add files via upload

--- src/main/java/com/example/library/Main.java
+++ src/main/java/com/example/library/Main.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 400);
 
         stage.setTitle("Hello!");
