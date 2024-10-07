845496e523bc4598a7d9c8c5003992d8de26b224
Hungpham711 <viethungbi711@gmail.com>
2024-10-07 13:14:22 +0700

    Add CreateAccount

---
+++ src/main/java/com/example/library/WindowManager.java
@@ -0,0 +1,41 @@
+package com.example.library;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class WindowManager {
+    private static Stage stage;
+    private static Scene scene;
+
+    public static void setStage(Stage newStage) {
+        stage =  newStage;
+    }
+
+    public static void setScene(Scene newScene) {
+        scene = newScene;
+    }
+
+    public static void addFXML(String fxmlFile, int width, int height) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxmlFile));
+        Scene scene = new Scene(fxmlLoader.load(), width, height);
+        stage.setTitle("Ulib Library Management");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void addFxmlCss(String fxmlFile, String cssMainFile, String cssSubFile, int width, int height) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxmlFile));
+        Scene scene = new Scene(fxmlLoader.load(), width, height);
+        scene.getStylesheets().add(Objects.requireNonNull(WindowManager.class.getResource(cssMainFile)).toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(WindowManager.class.getResource(cssSubFile)).toExternalForm());
+
+        stage.setTitle("Ulib Library Management");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
