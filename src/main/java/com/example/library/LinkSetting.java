08ff96e66698cafad65ef45d04deb8137890af7f
Pham Huy Hieu <hieuhuy160105@gmail.com>
2024-11-22 15:03:50 +0700

    commit for branch merging

--- src/main/java/com/example/library/LinkSetting.java
+++ src/main/java/com/example/library/LinkSetting.java
@@ -4,15 +4,15 @@ public enum LinkSetting {
 
     API_URL("AIzaSyCZ9Ee9DuenXY5gvW6IcKh_NhTMnB5U4qM"),
 
-    DATABASE_USER_URL("jdbc:mysql://localhost:3307/useraccount"),
+    DATABASE_USER_URL("jdbc:mysql://localhost:3306/librarymanagement"),
     DATABASE_USER_NAME("root"),
-    DATABASE_USER_PASSWORD("bongbibo9"),
+    DATABASE_USER_PASSWORD("Hieu@123456"),
 
-    DATABASE_MANAGER_URL("jdbc:mysql://localhost:3307/manageraccount"),
+    DATABASE_MANAGER_URL("jdbc:mysql://localhost:3306/librarymanagement"),
     DATABASE_MANAGER_NAME("root"),
-    DATABASE_MANAGER_PASSWORD("bongbibo9"),
+    DATABASE_MANAGER_PASSWORD("Hieu@123456"),
 
-    MANAGER_LIST_FILE_PATH("C:\\YEAR 2\\OOP\\JavaFX\\Bai tap lon _ Thu VIen\\data\\ListOfManagers.txt"),
+    MANAGER_LIST_FILE_PATH("D:\\LibraryManagement\\LibraryManagement\\data\\ListOfManagers.txt"),
 
 
     IMAGE_NULL("/com/example/library/assets/Picture_is_not_available.png"),
